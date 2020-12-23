a764e145086a7a10d76d90ba5b24cb4f22eece76
heedo-wang <dhkdgmleh@gmail.com>
2020-12-24 03:34:46 +0900

    main page poster

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const API_KEY = "d940557c0bf7f469e9e38909218018f7"
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,51 @@
+import React,{useEffect,useState} from 'react'
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
+import axios from'axios'
+function LandingPage() {
+const [Movies, setMovies] = useState([])
+const [MainMovieImage, setMainMovieImage] = useState(null)
+    useEffect(() => {
+        // const endpoint =`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+
+        // fetch(endpoint)
+        // .then(response=>response.json())
+        // .then(response =>{
+        //     console.log(response)
+        //     setMovies([...response.results])
+        //     setMainMovieImage(response.results[0])
+        // })
+
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+         axios.get(endpoint)
+            .then(response=>{
+                console.log(response.data)
+                    setMovies([...response.data.results])
+                    setMainMovieImage(response.data.results[0])
+            })
+
+    }, [])
+    return (
+        <div style={{width: '100%',margin:'0'}}>
+            {/* Main Image */}
+            {MainMovieImage && <MainImage image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+            title={MainMovieImage.original_title}
+            text={MainMovieImage.overview}
+            />
+        }
+            <div style={{width:'85%',margin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                    <hr/>
+
+                    {/* {Movie Grid} */}
+                    
+
+            </div>
+            <div style={{display:'flex',justifyContent:'center'}}>
+                <button>Load More</button>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
