aaa94c1e4ae571fc192348719fc7cbc86583552b
heedo-wang <dhkdgmleh@gmail.com>
2020-12-24 15:12:58 +0900

    Movie Detail Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,9 +2,13 @@ import React,{useEffect,useState} from 'react'
 import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
 import axios from'axios'
+import GridCards from '../commons/GridCards';
+import{Row} from 'antd';
+
 function LandingPage() {
 const [Movies, setMovies] = useState([])
 const [MainMovieImage, setMainMovieImage] = useState(null)
+const [CurrentPage, setCurrentPage] = useState(0)
     useEffect(() => {
         // const endpoint =`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 
@@ -17,14 +21,25 @@ const [MainMovieImage, setMainMovieImage] = useState(null)
         // })
 
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-         axios.get(endpoint)
-            .then(response=>{
-                console.log(response.data)
-                    setMovies([...response.data.results])
-                    setMainMovieImage(response.data.results[0])
-            })
+        fetchMovies(endpoint);
 
     }, [])
+
+    const fetchMovies = (endpoint) =>{
+        axios.get(endpoint)
+        .then(response=>{
+            console.log(response.data)
+                setMovies([...Movies, ...response.data.results])
+                setMainMovieImage(response.data.results[0])
+                setCurrentPage(response.data.page)
+                
+        })
+    }
+
+    const loadMoreItems =() =>{
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage+1}`
+        fetchMovies(endpoint)
+    }
     return (
         <div style={{width: '100%',margin:'0'}}>
             {/* Main Image */}
@@ -38,11 +53,20 @@ const [MainMovieImage, setMainMovieImage] = useState(null)
                     <hr/>
 
                     {/* {Movie Grid} */}
-                    
-
+            <Row gutter={[16,16]}>
+                {Movies && Movies.map((movie,index)=>(
+                    <React.Fragment key={index}>
+                        <GridCards image={movie.poster_path ? 
+                            `${IMAGE_BASE_URL}w500${movie.poster_path}` :null}
+                            movieId={movie.id}
+                            movieName={movie.original_title}
+                    />
+                    </React.Fragment>
+                ))}
+            </Row>
             </div>
             <div style={{display:'flex',justifyContent:'center'}}>
-                <button>Load More</button>
+                <button onClick={loadMoreItems}>Load More</button>
             </div>
         </div>
     )
